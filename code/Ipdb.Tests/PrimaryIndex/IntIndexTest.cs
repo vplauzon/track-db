1c081f0669a2c596745db3a28158dc1224734883
vplauzon <vincent_lauzon@yahoo.com.au>
2025-06-08 16:59:39 -0400

    .

---
+++ code/Ipdb.Tests/PrimaryIndex/IntIndexTest.cs
@@ -0,0 +1,18 @@
+using Ipdb.Lib;
+
+namespace Ipdb.Tests.PrimaryIndex
+{
+    public class IntIndexTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+            
+            // Act
+            
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
