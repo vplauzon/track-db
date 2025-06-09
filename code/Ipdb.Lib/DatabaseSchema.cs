8a1b52f5b9f172fb6f15afbf241903b80b33e4a1
vplauzon <vincent_lauzon@yahoo.com.au>
2025-06-09 13:28:00 -0400

    .

--- code/Ipdb.Lib/DatabaseSchema.cs
+++ code/Ipdb.Lib/DatabaseSchema.cs
@@ -1,18 +1,27 @@
 ﻿using System;
+using System.Collections.Immutable;
 
 namespace Ipdb.Lib
 {
     public class DatabaseSchema
     {
+        private readonly IImmutableList<(string Name, object Schema)> _tables;
+
         #region Constructor
         public DatabaseSchema()
+            : this(ImmutableArray<(string Name, object Schema)>.Empty)
         {
+        }
+
+        private DatabaseSchema(IImmutableList<(string Name, object Schema)> tables)
+        {
+            _tables = tables;
         }
         #endregion
 
         public DatabaseSchema AddTable<T>(string tableName, TableSchema<T> schema)
         {
-            throw new NotImplementedException();
+            return new DatabaseSchema(_tables.Add((tableName, schema)));
         }
     }
 }
