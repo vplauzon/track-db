237e440bea5462900f2cbb7bfb307ce544103b7d
vplauzon <vincent_lauzon@yahoo.com.au>
2025-06-09 13:26:27 -0400

    .

--- code/Ipdb.Lib/DatabaseSchema.cs
+++ code/Ipdb.Lib/DatabaseSchema.cs
@@ -4,6 +4,12 @@ namespace Ipdb.Lib
 {
     public class DatabaseSchema
     {
+        #region Constructor
+        public DatabaseSchema()
+        {
+        }
+        #endregion
+
         public DatabaseSchema AddTable<T>(string tableName, TableSchema<T> schema)
         {
             throw new NotImplementedException();
