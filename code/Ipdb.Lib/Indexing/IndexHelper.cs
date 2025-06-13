a28fa5f32e1bbd135c3525d092e0f3485a54526a
vplauzon <vincent_lauzon@yahoo.com.au>
2025-06-12 20:40:29 -0400

    .

---
+++ code/Ipdb.Lib/Indexing/IndexHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ipdb.Lib.Indexing
+{
+    internal static class IndexHelper
+    {
+        public static string ToPath(this Expression expression)
+        {
+            return "Bob";
+        }
+    }
+}
---
+++ code/Ipdb.Lib/Querying/IIndexEqual.cs
@@ -0,0 +1,9 @@
+﻿namespace Ipdb.Lib.Querying
+{
+    internal interface IIndexEqual<T>
+    {
+        IndexDefinition<T> IndexDefinition{ get; }
+
+        short KeyHash { get; }
+    }
+}
