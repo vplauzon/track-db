23244847930e617024d96f54ff4628ca4ad02c76
vplauzon <vincent_lauzon@yahoo.com.au>
2025-06-15 17:12:22 -0400

    .

---
+++ code/Ipdb.Lib/Cache/TransactionCache.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ipdb.Lib.Cache
+{
+    internal record TransactionCache(DatabaseCache DatabaseCache, TransactionLog TransactionLog);
+}
