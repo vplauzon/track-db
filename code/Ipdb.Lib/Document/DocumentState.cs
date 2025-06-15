465c821ef26ede09b7e2a6ba3066faa51597103a
vplauzon <vincent_lauzon@yahoo.com.au>
2025-06-14 22:05:47 -0400

    .

---
+++ code/Ipdb.Lib/Document/DocumentState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ipdb.Lib.Document
+{
+    /// <summary>
+    /// State of <see cref="DocumentManager"/>> within a transaction.
+    /// </summary>
+    internal class DocumentState
+    {
+    }
+}
---
+++ code/Ipdb.Lib/Indexing/IndexState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ipdb.Lib.Indexing
+{
+    /// <summary>
+    /// State of <see cref="IndexManager"/>> within a transaction.
+    /// </summary>
+    internal class IndexState
+    {
+    }
+}
