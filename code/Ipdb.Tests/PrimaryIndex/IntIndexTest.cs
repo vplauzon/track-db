4c164571da2b585887e3ff4b4dc2f1a0f53e5c7f
vplauzon <vincent_lauzon@yahoo.com.au>
2025-06-18 18:12:18 -0400

    .

--- code/Ipdb.Tests/PrimaryIndex/IntIndexTest.cs
+++ code/Ipdb.Tests/PrimaryIndex/IntIndexTest.cs
@@ -5,72 +5,68 @@ using System.Threading.Tasks;
 
 namespace Ipdb.Tests.PrimaryIndex
 {
-    public class IntIndexTest : BaseTest, IAsyncLifetime
+    public class IntIndexTest
     {
         #region Inner types
         private record MyDocument(int Id, string Text);
         #endregion
 
         private const string TABLE_NAME = "mydocs";
-        private Database? _db = null;
-
-        public async Task InitializeAsync()
-        {
-            _db = await Engine.LoadDatabaseAsync(
-                "mydb",
-                new DatabaseSchema()
-                .AddTable(TableSchema<MyDocument>.CreateSchema(TABLE_NAME, d => d.Id)));
-        }
-
-        public Task DisposeAsync() => Task.CompletedTask;
 
         [Fact]
-        public void InsertOnly()
+        public async Task InsertOnly()
         {
-            var table = CreateTable();
-            var doc = new MyDocument(1, "House");
+            using (var testTable = await CreateTestTableAsync())
+            {
+                var doc = new MyDocument(1, "House");
 
-            table.AppendDocument(doc);
+                testTable.Table.AppendDocument(doc);
+            }
         }
 
         [Fact]
-        public void InsertAndRetrieve()
+        public async Task InsertAndRetrieveAsync()
         {
-            var table = CreateTable();
-            var doc = new MyDocument(42, "House");
+            using (var testTable = await CreateTestTableAsync())
+            {
+                var doc = new MyDocument(42, "House");
 
-            table.AppendDocument(doc);
+                testTable.Table.AppendDocument(doc);
 
-            var retrievedDocs = table
-                .Query(table.QueryOp.Equal(d => d.Id, doc.Id))
-                .ToImmutableArray();
+                var retrievedDocs = testTable.Table
+                    .Query(testTable.Table.QueryOp.Equal(d => d.Id, doc.Id))
+                    .ToImmutableArray();
 
-            Assert.Single(retrievedDocs);
-            Assert.Equal(doc, retrievedDocs[0]);
+                Assert.Single(retrievedDocs);
+                Assert.Equal(doc, retrievedDocs[0]);
+            }
         }
 
         [Fact]
-        public void InsertUpdateAndRetrieve()
+        public async Task InsertUpdateAndRetrieveAsync()
         {
-            var table = CreateTable();
-            var doc1 = new MyDocument(1, "House");
-            var doc2 = new MyDocument(1, "Home");
+            using (var testTable = await CreateTestTableAsync())
+            {
+                var doc1 = new MyDocument(1, "House");
+                var doc2 = new MyDocument(1, "Home");
 
-            table.AppendDocument(doc1);
-            table.AppendDocument(doc2);
+                testTable.Table.AppendDocument(doc1);
+                testTable.Table.AppendDocument(doc2);
 
-            var retrievedDocs = table
-                .Query(table.QueryOp.Equal(d => d.Id, 1))
-                .ToImmutableArray();
+                var retrievedDocs = testTable.Table
+                    .Query(testTable.Table.QueryOp.Equal(d => d.Id, 1))
+                    .ToImmutableArray();
 
-            Assert.Single(retrievedDocs);
-            Assert.Equal(doc2.Id, retrievedDocs[0].Id);
-            Assert.Equal(doc2.Text, retrievedDocs[0].Text);
+                Assert.Single(retrievedDocs);
+                Assert.Equal(doc2.Id, retrievedDocs[0].Id);
+                Assert.Equal(doc2.Text, retrievedDocs[0].Text);
+            }
         }
 
-        private Table<MyDocument> CreateTable()
+        private async Task<TestTable<MyDocument>> CreateTestTableAsync()
         {
-            return _db!.GetTable<MyDocument>(TABLE_NAME);
+            return await TestTable.CreateAsync(
+                TableSchema<MyDocument>.CreateSchema(TABLE_NAME, d => d.Id));
         }
     }
 }
