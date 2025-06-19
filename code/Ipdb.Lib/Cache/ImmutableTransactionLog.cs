4305b8397642aa7328811bceb3cf9157ff15b640
vplauzon <vincent_lauzon@yahoo.com.au>
2025-06-18 21:57:50 -0400

    .

--- code/Ipdb.Lib/Cache/ImmutableTransactionLog.cs
+++ code/Ipdb.Lib/Cache/ImmutableTransactionLog.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Ipdb.Lib.Cache
@@ -25,9 +26,49 @@ namespace Ipdb.Lib.Cache
                 + DeletedIndexes.Values.Sum(c => c.Count);
         }
 
+        public TransactionLog ToTransactionLog()
+        {
+            var logs = new TransactionLog(
+                NewDocuments.ToImmutableDictionary().ToBuilder(),
+                DeletedDocuments.ToImmutableHashSet().ToBuilder(),
+                NewIndexes.ToImmutableDictionary(
+                    p => p.Key,
+                    p => p.Value.ToImmutableHashSet().ToBuilder()).ToBuilder(),
+                DeletedIndexes.ToImmutableDictionary(
+                    p => p.Key,
+                    p => p.Value.ToImmutableHashSet().ToBuilder()).ToBuilder());
+
+            return logs;
+        }
+
         public ImmutableTransactionLog Merge(ImmutableTransactionLog next)
         {
-            throw new NotImplementedException();
+            var log = ToTransactionLog();
+
+            foreach (var pair in next.NewDocuments)
+            {
+                log.AppendDocument(pair.Key, pair.Value);
+            }
+            foreach (var revisionId in next.DeletedDocuments)
+            {
+                log.DeleteDocument(revisionId);
+            }
+            foreach (var pair in next.NewIndexes)
+            {
+                foreach (var hash in pair.Value)
+                {
+                    log.AppendIndexValue(pair.Key, hash);
+                }
+            }
+            foreach (var pair in next.DeletedIndexes)
+            {
+                foreach (var hash in pair.Value)
+                {
+                    log.DeleteIndexValue(pair.Key, hash);
+                }
+            }
+
+            return log.ToImmutable();
         }
     }
 }
