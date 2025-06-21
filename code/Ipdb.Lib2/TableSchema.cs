c9198a08cbd79bcde3f29301bc5121cd9ebd5c43
vplauzon <vincent_lauzon@yahoo.com.au>
2025-06-21 15:56:48 -0400

    .

---
+++ code/Ipdb.Lib2/TableSchema.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ipdb.Lib2
+{
+    /// <summary>Schema of a table.</summary>
+    public abstract class TableSchema
+    {
+        protected TableSchema(string tableName, IImmutableList<string> primaryKeys)
+        {
+            TableName = tableName;
+            PrimaryKeys = primaryKeys;
+        }
+
+        public string TableName { get; }
+
+        public abstract Type RepresentationType { get; }
+
+        public IImmutableList<string> PrimaryKeys { get; }
+    }
+
+    /// <summary>
+    /// Schema of a table:  table name, the .NET type representing it and,
+    /// optionally, primary keys.
+    /// If primary key extractors are present, they point to one or many
+    /// properties within the representation types which values act
+    /// as a primary key.
+    /// If a record with the same primary exists in the table, the record is updated otherwise
+    /// inserted (update semantic).  If no primary key is defined, records are always inserted
+    /// (can be explicitly deleted).
+    /// Column type supported:  <see cref="int"/>.
+    /// </summary>
+    /// <typeparam name="T">Representation Type</typeparam>
+    public class TableSchema<T> : TableSchema
+    {
+        #region Constructors
+        /// <summary>Create a table schema.</summary>>
+        /// <param name="tableName"></param>
+        public TableSchema(string tableName)
+            : this(tableName, ImmutableArray<string>.Empty)
+        {
+        }
+
+        private TableSchema(string tableName, IImmutableList<string> primaryKeys)
+            : base(tableName, primaryKeys)
+        {
+        }
+        #endregion
+
+        public override Type RepresentationType => typeof(T);
+
+        /// <summary>
+        /// Add a primary key mapped to a property.
+        /// </summary>
+        /// <typeparam name="PT"></typeparam>
+        /// <param name="primaryKeyExtractor"></param>
+        /// <returns></returns>
+        public TableSchema<T> AddPrimaryKey<PT>(Expression<Func<T, PT>> primaryKeyExtractor)
+        {
+            if (primaryKeyExtractor.Body is MemberExpression me)
+            {
+                if (me.Member.MemberType == MemberTypes.Property)
+                {
+                    var propertyName = me.Member.Name;
+
+                    return new TableSchema<T>(TableName, PrimaryKeys.Add(propertyName));
+                }
+                else
+                {
+                    throw new NotSupportedException(
+                        $"Primary key expression only supports properties:  '{primaryKeyExtractor}'");
+                }
+            }
+            else
+            {
+                throw new NotSupportedException(
+                    $"Primary key expression not supported:  '{primaryKeyExtractor}'");
+            }
+        }
+    }
+}
---
+++ code/Ipdb.Tests2/TableSchemaTest.cs
@@ -0,0 +1,28 @@
+﻿using Ipdb.Lib2;
+using Xunit;
+
+namespace Ipdb.Tests2
+{
+    public class TableSchemaTest
+    {
+        #region Inner types
+        private readonly record struct OnlyInt(int Integer);
+        #endregion
+
+        [Fact]
+        public void TestOnlyInt()
+        {
+            const string TABLE_NAME = "MyTable";
+            var schema = new TableSchema<OnlyInt>(TABLE_NAME);
+
+            Assert.Equal(TABLE_NAME, schema.TableName);
+            Assert.Equal(typeof(OnlyInt), schema.RepresentationType);
+            Assert.Empty(schema.PrimaryKeys);
+
+            schema = schema.AddPrimaryKey(o => o.Integer);
+
+            Assert.Single(schema.PrimaryKeys);
+            Assert.Equal(nameof(OnlyInt.Integer), schema.PrimaryKeys[0]);
+        }
+    }
+}
