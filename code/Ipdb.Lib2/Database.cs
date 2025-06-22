3212c7c2ac1ec2843052b023d36f7b22aa2b67cc
vplauzon <vincent_lauzon@yahoo.com.au>
2025-06-22 16:03:22 -0400

    .

---
+++ code/Ipdb.Lib2/Database.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ipdb.Lib2
+{
+    /// <summary>
+    /// Database:  a collection of tables that can share transactions
+    /// and are persisted in the same file.
+    /// </summary>
+    public class Database
+    {
+        #region Constructors
+        public Database(string databasePath, params IEnumerable<TableSchema>)
+        {
+        }
+        #endregion
+    }
+}
