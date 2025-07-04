89cce42cf13f197e2057be349ca8eb7ca4af6e5a
vplauzon <vincent_lauzon@yahoo.com.au>
2025-07-03 23:24:02 -0400

    .

--- code/Ipdb.Lib2/Cache/CachedBlock/BlockBuilder.cs
+++ code/Ipdb.Lib2/Cache/CachedBlock/BlockBuilder.cs
@@ -1,16 +1,34 @@
 ﻿using System;
 using System.Collections.Immutable;
+using System.Linq;
 
 namespace Ipdb.Lib2.Cache.CachedBlock
 {
     internal class BlockBuilder
     {
         private readonly TableSchema _schema;
+        private readonly IImmutableList<ICachedColumn> _columns;
 
+        #region Constructors
         public BlockBuilder(TableSchema schema)
         {
             _schema = schema;
+            _columns = _schema.Columns
+                .Select(c => CreateCachedColumn(c.ColumnType))
+                .Append(CreateRecordIdColumn())
+                .ToImmutableArray();
         }
+
+        private static ICachedColumn CreateCachedColumn(Type columnType)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static ICachedColumn CreateRecordIdColumn()
+        {
+            return new SimpleCachedColumn<long>();
+        }
+        #endregion
 
         public bool IsEmpty => throw new NotImplementedException();
 
---
+++ code/Ipdb.Lib2/Cache/CachedBlock/SimpleCachedColumn.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ipdb.Lib2.Cache.CachedBlock
+{
+    internal class SimpleCachedColumn<T> : ICachedColumn
+        where T : IEquatable<T>, IComparable<T>
+    {
+    }
+}
