a2e29cbf61154c9d30a6247f506c1de0e4d878c4
vplauzon <vincent_lauzon@yahoo.com.au>
2025-08-15 18:50:01 -0400

    .

--- code/Ipdb.Tests2/Codecs/Int64CodecTest.cs
+++ code/Ipdb.Tests2/Codecs/Int64CodecTest.cs
@@ -54,6 +54,84 @@ namespace Ipdb.Tests2.Codecs
                 true);
         }
 
+        [Fact]
+        public void EmptySequence()
+        {
+            Assert.Throws<ArgumentNullException>(() => Int64Codec.Compress(null!));
+            Assert.Throws<ArgumentNullException>(() => Int64Codec.Compress(Array.Empty<long?>()));
+        }
+
+        [Fact]
+        public void SequenceTooLarge()
+        {
+            var data = Enumerable.Range(0, UInt16.MaxValue + 1)
+                .Select(i => (long?)i)
+                .ToArray();
+            Assert.Throws<ArgumentOutOfRangeException>(() => Int64Codec.Compress(data));
+        }
+
+        [Fact]
+        public void SingleValueSequences()
+        {
+            TestScenario(new long?[] { 42 }, false);
+            TestScenario(new long?[] { null }, false);
+        }
+
+        [Fact]
+        public void TwoValueSequences()
+        {
+            TestScenario(new long?[] { 1, 2 }, true);
+            TestScenario(new long?[] { 1, null }, true);
+            TestScenario(new long?[] { null, 1 }, true);
+            TestScenario(new long?[] { 1, 1 }, false);
+        }
+
+        [Fact]
+        public void BoundaryValues()
+        {
+            TestScenario(new long?[] { long.MinValue, 0, long.MaxValue }, true);
+            TestScenario(new long?[] { long.MinValue, null, long.MaxValue }, true);
+            
+            // Large deltas
+            TestScenario(new long?[] { 0, 1000000000, 2000000000, 3000000000 }, true);
+            
+            // Values requiring maximum bit width
+            TestScenario(new long?[] { long.MinValue, long.MinValue / 2, 0, 
+                long.MaxValue / 2, long.MaxValue }, true);
+        }
+
+        [Fact]
+        public void BitmapEdgeCases()
+        {
+            // Exactly 8 items (one byte bitmap)
+            TestScenario(new long?[] { 1, null, 2, null, 3, null, 4, null }, true);
+            
+            // 9 items (spans two bytes)
+            TestScenario(new long?[] { 1, null, 2, null, 3, null, 4, null, 5 }, true);
+            
+            // Null at byte boundary
+            TestScenario(new long?[] { 1, 2, 3, 4, 5, 6, 7, null, 9, 10 }, true);
+        }
+
+        [Fact]
+        public void CompressionEfficiency()
+        {
+            // Small deltas
+            TestScenario(new long?[] { 1000, 1001, 1002, 1003, 1004 }, true);
+            
+            // Large deltas
+            TestScenario(new long?[] { 1000, 2000, 3000, 4000, 5000 }, true);
+            
+            // Alternating pattern
+            TestScenario(new long?[] { 1, null, 2, null, 3, null, 4, null }, true);
+            
+            // Mostly null
+            TestScenario(new long?[] { null, null, null, 42, null, null }, true);
+            
+            // Mostly values
+            TestScenario(new long?[] { 1, 2, 3, null, 4, 5, 6 }, true);
+        }
+
         private static void TestScenario(IEnumerable<long?> data, bool doExpectPayload)
         {
             var bundle = Int64Codec.Compress(data);
