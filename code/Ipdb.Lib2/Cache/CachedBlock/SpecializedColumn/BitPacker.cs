98eb290809cb24c5c5e1365de3cf8da46ae8d32c
vplauzon <vincent_lauzon@yahoo.com.au>
2025-08-15 13:14:39 -0400

    .

--- code/Ipdb.Lib2/Cache/CachedBlock/SpecializedColumn/BitPacker.cs
+++ code/Ipdb.Lib2/Cache/CachedBlock/SpecializedColumn/BitPacker.cs
@@ -22,7 +22,99 @@ namespace Ipdb.Lib2.Cache.CachedBlock.SpecializedColumn
         /// <returns>Byte array of bit-packed representation of <paramref name="data"/>.</returns>
         public static byte[] Pack(IEnumerable<long> data, int dataCount, long maximumValue)
         {
-            throw new NotImplementedException();
+            // Calculate number of bits needed per value
+            var bitsPerValue = (int)Math.Ceiling(Math.Log2(maximumValue + 1));
+            // Calculate total bits and bytes needed
+            var totalBits = dataCount * bitsPerValue;
+            var totalBytes = (totalBits + 7) / 8; // Round up to nearest byte
+            var result = new byte[totalBytes];
+            var currentBitPosition = 0;
+            
+            foreach (long value in data)
+            {
+                // Calculate which byte(s) this value's bits will go into
+                var startByteIndex = currentBitPosition / 8;
+                var bitOffsetInStartByte = currentBitPosition % 8;
+                // Write the value's bits into the byte array
+                var remainingBits = bitsPerValue;
+                var remainingValue = value;
+                
+                while (remainingBits > 0)
+                {
+                    // Calculate how many bits we can write to the current byte
+                    var bitsToWrite = Math.Min(8 - bitOffsetInStartByte, (int)remainingBits);
+                    
+                    // Extract the bits we want to write
+                    var mask = (1L << bitsToWrite) - 1;
+                    var bits = (byte)(remainingValue & mask);
+                    
+                    // Shift the bits to their correct position in the byte
+                    bits = (byte)(bits << bitOffsetInStartByte);
+                    
+                    // Combine with existing bits in the byte using OR
+                    result[startByteIndex] |= bits;
+                    
+                    // Update our position trackers
+                    remainingValue >>= bitsToWrite;
+                    remainingBits -= bitsToWrite;
+                    startByteIndex++;
+                    bitOffsetInStartByte = 0;
+                }
+                
+                currentBitPosition += bitsPerValue;
+            }
+            
+            return result;
+        }
+
+        /// <summary>
+        /// Unpacks a sequence of bit-packed values back into their original long values.
+        /// </summary>
+        /// <param name="data">The byte array containing bit-packed values.</param>
+        /// <param name="dataCount">The number of values to unpack.</param>
+        /// <param name="maximumValue">The maximum possible value in the original data.</param>
+        /// <returns>Array of unpacked long values.</returns>
+        public static long[] Unpack(byte[] data, int dataCount, long maximumValue)
+        {
+            // Calculate number of bits per value (same as Pack method)
+            var bitsPerValue = (int)Math.Ceiling(Math.Log2(maximumValue + 1));
+            var result = new long[dataCount];
+            var currentBitPosition = 0;
+            
+            for (var i = 0; i < dataCount; i++)
+            {
+                // Calculate which byte we start reading from
+                var startByteIndex = currentBitPosition / 8;
+                var bitOffsetInStartByte = currentBitPosition % 8;
+                var remainingBits = bitsPerValue;
+                var value = 0L;
+                var bitsProcessed = 0;
+                
+                while (remainingBits > 0)
+                {
+                    // Calculate how many bits we can read from current byte
+                    var bitsToRead = Math.Min(8 - bitOffsetInStartByte, remainingBits);
+                    
+                    // Extract bits from current byte
+                    var currentByte = data[startByteIndex];
+                    var mask = (1 << bitsToRead) - 1;
+                    var bits = (currentByte >> bitOffsetInStartByte) & mask;
+                    
+                    // Add these bits to our value
+                    value |= ((long)bits << bitsProcessed);
+                    
+                    // Update our trackers
+                    remainingBits -= bitsToRead;
+                    bitsProcessed += bitsToRead;
+                    startByteIndex++;
+                    bitOffsetInStartByte = 0;
+                }
+                
+                result[i] = value;
+                currentBitPosition += bitsPerValue;
+            }
+            
+            return result;
         }
     }
 }
---
+++ code/Ipdb.Tests2/Codecs/BitPackerTest.cs
@@ -0,0 +1,44 @@
+﻿using Ipdb.Lib2.Cache.CachedBlock.SpecializedColumn;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Ipdb.Tests2.Codecs
+{
+    public class BitPackerTest
+    {
+        [Fact]
+        public void Scenarios()
+        {
+            var random = new Random();
+            var scenarios = new IEnumerable<long>[]
+            {
+                new long[] { 0, 0, 0, 1 },
+                new long[] { 0, 1, 2, 3, 4, 5, 6 },
+                new long[] { 0, 42 },
+                Enumerable.Range(0, 25000)
+                .Select(i=>(long)random.Next(0, 25000))
+                .ToImmutableArray(),
+            };
+
+            foreach (var originalSequence in scenarios)
+            {
+                var packedArray = BitPacker.Pack(
+                    originalSequence,
+                    originalSequence.Count(),
+                    originalSequence.Max());
+                var unpackedArray = BitPacker.Unpack(
+                    packedArray,
+                    originalSequence.Count(),
+                    originalSequence.Max())
+                    .ToImmutableArray();
+
+                Assert.True(Enumerable.SequenceEqual(unpackedArray, originalSequence));
+            }
+        }
+    }
+}
