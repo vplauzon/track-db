377ab235bc8b3afd25db6dfaffd0b0c9d5b37356
vplauzon <vincent_lauzon@yahoo.com.au>
2025-08-15 12:51:25 -0400

    .

---
+++ code/Ipdb.Lib2/Cache/CachedBlock/SpecializedColumn/BitPacker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ipdb.Lib2.Cache.CachedBlock.SpecializedColumn
+{
+    internal static class BitPacker
+    {
+        /// <summary>
+        /// Packs a sequence of <see cref="long"/>s.  Each item is encoded into a sequence of bits.
+        /// Number of bits per item is dictated by the range of value, which is 0 to
+        /// <paramref name="maximumValue"/>.
+        /// </summary>
+        /// <param name="data">
+        /// Sequence of <see cref="long"/> items with minimum value of 0 and maximum value of
+        /// <paramref name="maximumValue"/>.
+        /// </param>
+        /// <param name="dataCount">The number of items in <paramref name="data"/>.</param>
+        /// <param name="maximumValue">Maximum value in <paramref name="data"/>.</param>
+        /// <returns>Byte array of bit-packed representation of <paramref name="data"/>.</returns>
+        public static byte[] Pack(IEnumerable<long> data, int dataCount, long maximumValue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
