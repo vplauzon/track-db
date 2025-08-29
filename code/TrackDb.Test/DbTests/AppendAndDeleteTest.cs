81a290a9ae52ea9c5758821849f00da2d9588e70
vplauzon <vincent_lauzon@yahoo.com.au>
2025-08-28 20:37:20 -0400

    .

--- code/TrackDb.Test/DbTests/AppendAndDeleteTest.cs
+++ code/TrackDb.Test/DbTests/AppendAndDeleteTest.cs
@@ -7,13 +7,13 @@ using Xunit;
 
 namespace TrackDb.Test.DbTests
 {
-    public class AppendOneRecordAndDeleteTest
+    public class AppendAndDeleteTest
     {
         [Theory]
         [InlineData(false, false)]
         [InlineData(true, false)]
         [InlineData(true, true)]
-        public async Task IntOnly(bool doPushPendingData, bool doHardDelete)
+        public async Task OneRecord(bool doPushPendingData, bool doHardDelete)
         {
             await using (var db = new TestDatabase())
             {
@@ -29,6 +29,27 @@ namespace TrackDb.Test.DbTests
                 await db.ForceDataManagementAsync(doHardDelete
                     ? DataManagementActivity.HardDeleteAll
                     : DataManagementActivity.None);
+            }
+        }
+
+        [Theory]
+        [InlineData(false)]
+        [InlineData(true)]
+        public async Task MultipleRecords(bool doPushPendingData)
+        {
+            await using (var db = new TestDatabase())
+            {
+                db.IntOnlyTable.AppendRecord(new TestDatabase.IntOnly(1));
+                db.IntOnlyTable.AppendRecord(new TestDatabase.IntOnly(2));
+                db.IntOnlyTable.AppendRecord(new TestDatabase.IntOnly(3));
+                db.IntOnlyTable.AppendRecord(new TestDatabase.IntOnly(4));
+                await db.ForceDataManagementAsync(doPushPendingData
+                    ? DataManagementActivity.PersistAllData
+                    : DataManagementActivity.None);
+
+                db.IntOnlyTable.Query()
+                    .Where(db.IntOnlyTable.PredicateFactory.Equal(r => r.Integer, 1))
+                    .Delete();
             }
         }
     }
--- code/TrackDb.Test/DbTests/AppendOnlyTest.cs
+++ code/TrackDb.Test/DbTests/AppendOnlyTest.cs
@@ -24,5 +24,21 @@ namespace TrackDb.Test.DbTests
                     : DataManagementActivity.None);
             }
         }
+
+        [Theory]
+        [InlineData(false)]
+        [InlineData(true)]
+        public async Task MultipleRecords(bool doPushPendingData)
+        {
+            await using (var db = new TestDatabase())
+            {
+                db.IntOnlyTable.AppendRecord(new TestDatabase.IntOnly(1));
+                db.IntOnlyTable.AppendRecord(new TestDatabase.IntOnly(2));
+                db.IntOnlyTable.AppendRecord(new TestDatabase.IntOnly(3));
+                await db.ForceDataManagementAsync(doPushPendingData
+                    ? DataManagementActivity.PersistAllData
+                    : DataManagementActivity.None);
+            }
+        }
     }
 }
