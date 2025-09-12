368b79c915edcca5a2b298f9b394f3d416da9553
vplauzon <vincent_lauzon@yahoo.com.au>
2025-09-12 16:52:24 -0400

    .

--- deployment/patch-version.py
+++ deployment/patch-version.py
@@ -8,24 +8,17 @@ def writeXml(document, path):
     with open(path, "w" ) as fs:  
         fs.write(document.toxml() ) 
 
-if len(sys.argv) != 4:
+if len(sys.argv) != 3:
     print("There are %d arguments" % (len(sys.argv)-1))
     print("Arguments should be")
     print("1-File path")
     print("2-Patch number")
-    print("3-Alpha (true or false)")
 
     exit(1)
 else:
     path = sys.argv[1]
     patchNumber = sys.argv[2]
-    isAlpha = sys.argv[3]=='true'
-
-    if isAlpha:
-        suffix = '-alpha'
-    else:
-        suffix = ''
-
+    
     print ('Text file:  %s' % (path))
     print ('Patch Number:  %s' % (patchNumber))
 
