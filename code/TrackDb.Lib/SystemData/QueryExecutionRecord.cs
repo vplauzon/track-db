ac6090f610e35c41bb13ee0224eff3e4c73245eb
vplauzon <vincent_lauzon@yahoo.com.au>
2025-09-26 15:52:37 -0400

    .

---
+++ code/TrackDb.Lib/SystemData/QueryExecutionRecord.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackDb.Lib.SystemData
+{
+    public record QueryExecutionRecord(
+        DateTime Timestamp,
+        string QueryId,
+        string QueryTag,
+        long BlockId,
+        string Predicate);
+}
