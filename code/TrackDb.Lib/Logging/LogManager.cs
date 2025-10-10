e347da080518c1588cf337222bb99e34bed2a031
vplauzon <vincent_lauzon@yahoo.com.au>
2025-10-09 21:48:36 -0400

    .

--- code/TrackDb.Lib/Logging/LogManager.cs
+++ code/TrackDb.Lib/Logging/LogManager.cs
@@ -17,68 +17,19 @@ namespace TrackDb.Lib.Logging
         private static readonly string SEPARATOR = "\n";
 
         private readonly LogPolicy _logPolicy;
-        private readonly DataLakeDirectoryClient _loggingDirectory;
-        private readonly BlobContainerClient _loggingContainer;
+        private readonly LogStorageManager _logStorageManager;
         private readonly ConcurrentQueue<string> _contentQueue = new();
         private volatile int _contentLength;
-        private long _currentLogBlobIndex = 1;
-        private AppendBlobClient? _currentLogBlob;
 
         public LogManager(LogPolicy logPolicy)
         {
-            if (logPolicy.StorageConfiguration == null)
-            {
-                throw new ArgumentNullException(nameof(logPolicy.StorageConfiguration));
-            }
-
             _logPolicy = logPolicy;
-            if (_logPolicy.StorageConfiguration.TokenCredential != null)
-            {
-                var dummyBlob = new AppendBlobClient(
-                    _logPolicy.StorageConfiguration.LogFolderUri,
-                    _logPolicy.StorageConfiguration.TokenCredential);
-
-                _loggingDirectory = new DataLakeDirectoryClient(
-                    _logPolicy.StorageConfiguration.LogFolderUri,
-                    _logPolicy.StorageConfiguration.TokenCredential);
-                _loggingContainer = dummyBlob.GetParentBlobContainerClient();
-            }
-            else
-            {
-                var dummyBlob = new AppendBlobClient(
-                    _logPolicy.StorageConfiguration.LogFolderUri,
-                    _logPolicy.StorageConfiguration.SasCredential);
-
-                _loggingDirectory = new DataLakeDirectoryClient(
-                    _logPolicy.StorageConfiguration.LogFolderUri,
-                    _logPolicy.StorageConfiguration.SasCredential);
-                _loggingContainer = dummyBlob.GetParentBlobContainerClient();
-            }
+            _logStorageManager = new LogStorageManager(logPolicy);
         }
 
         public async Task InitLogsAsync(CancellationToken ct)
         {
-            await _loggingDirectory.CreateIfNotExistsAsync(cancellationToken: ct);
-
-            var blobList = await _loggingDirectory.GetPathsAsync(cancellationToken: ct)
-                .ToImmutableListAsync();
-            var lastCheckpoint = blobList
-                .Select(i => i.Name)
-                .Where(n => n.StartsWith("checkpoint-"))
-                .Where(n => n.EndsWith(".json"))
-                .OrderBy(n => n)
-                .LastOrDefault();
-
-            if (lastCheckpoint != null)
-            {
-                throw new NotImplementedException();
-            }
-            else
-            {
-                _currentLogBlob = _loggingContainer.GetAppendBlobClient(
-                    _loggingDirectory.GetFileClient($"log-{_currentLogBlobIndex:D19}.json").Path);
-                await _currentLogBlob.CreateIfNotExistsAsync();
-            }
+            await _logStorageManager.InitLogsAsync(ct);
         }
 
         public void QueueContent(string contentText)
@@ -89,9 +40,9 @@ namespace TrackDb.Lib.Logging
                 Interlocked.Add(ref _contentLength, contentText.Length + SEPARATOR.Length);
         }
 
-        ValueTask IAsyncDisposable.DisposeAsync()
+        async ValueTask IAsyncDisposable.DisposeAsync()
         {
-            throw new NotImplementedException();
+            await ((IAsyncDisposable)_logStorageManager).DisposeAsync();
         }
     }
 }
---
+++ code/TrackDb.Lib/Logging/LogStorageManager.cs
@@ -0,0 +1,82 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Specialized;
+using Azure.Storage.Files.DataLake;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using TrackDb.Lib.Policies;
+
+namespace TrackDb.Lib.Logging
+{
+    internal class LogStorageManager : IAsyncDisposable
+    {
+        private readonly LogPolicy _logPolicy;
+        private readonly DataLakeDirectoryClient _loggingDirectory;
+        private readonly BlobContainerClient _loggingContainer;
+        private long _currentLogBlobIndex = 1;
+        private AppendBlobClient? _currentLogBlob;
+
+        public LogStorageManager(LogPolicy logPolicy)
+        {
+            if (logPolicy.StorageConfiguration == null)
+            {
+                throw new ArgumentNullException(nameof(logPolicy.StorageConfiguration));
+            }
+
+            _logPolicy = logPolicy;
+            if (_logPolicy.StorageConfiguration.TokenCredential != null)
+            {
+                var dummyBlob = new AppendBlobClient(
+                    _logPolicy.StorageConfiguration.LogFolderUri,
+                    _logPolicy.StorageConfiguration.TokenCredential);
+
+                _loggingDirectory = new DataLakeDirectoryClient(
+                    _logPolicy.StorageConfiguration.LogFolderUri,
+                    _logPolicy.StorageConfiguration.TokenCredential);
+                _loggingContainer = dummyBlob.GetParentBlobContainerClient();
+            }
+            else
+            {
+                var dummyBlob = new AppendBlobClient(
+                    _logPolicy.StorageConfiguration.LogFolderUri,
+                    _logPolicy.StorageConfiguration.SasCredential);
+
+                _loggingDirectory = new DataLakeDirectoryClient(
+                    _logPolicy.StorageConfiguration.LogFolderUri,
+                    _logPolicy.StorageConfiguration.SasCredential);
+                _loggingContainer = dummyBlob.GetParentBlobContainerClient();
+            }
+        }
+
+        async ValueTask IAsyncDisposable.DisposeAsync()
+        {
+            await Task.CompletedTask;
+        }
+
+        public async Task InitLogsAsync(CancellationToken ct)
+        {
+            await _loggingDirectory.CreateIfNotExistsAsync(cancellationToken: ct);
+
+            var blobList = await _loggingDirectory.GetPathsAsync(cancellationToken: ct)
+                .ToImmutableListAsync();
+            var lastCheckpoint = blobList
+                .Select(i => i.Name)
+                .Where(n => n.StartsWith("checkpoint-"))
+                .Where(n => n.EndsWith(".json"))
+                .OrderBy(n => n)
+                .LastOrDefault();
+
+            if (lastCheckpoint != null)
+            {
+                throw new NotImplementedException();
+            }
+            else
+            {
+                _currentLogBlob = _loggingContainer.GetAppendBlobClient(
+                    _loggingDirectory.GetFileClient($"log-{_currentLogBlobIndex:D19}.json").Path);
+                await _currentLogBlob.CreateIfNotExistsAsync();
+            }
+        }
+    }
+}
