2b583a218f89752d7bd8fdaa43bfe3741835070f
vplauzon <vincent_lauzon@yahoo.com.au>
2025-11-04 20:11:33 -0500

    .

--- code/TrackDb.UnitTest/DbTests/MetaMetaTest.cs
+++ code/TrackDb.UnitTest/DbTests/MetaMetaTest.cs
@@ -10,7 +10,7 @@ namespace TrackDb.UnitTest.DbTests
     public class MetaMetaTest
     {
         [Fact]
-        public async Task MetaMetaTable()
+        public async Task AppendOnly()
         {
             await using (var db = await TestDatabase.CreateAsync())
             {
@@ -28,12 +28,95 @@ namespace TrackDb.UnitTest.DbTests
                 Assert.NotNull(metadataTableName);
 
                 var metaMetadataTableName = map[metadataTableName].MetaDataTableName;
-            
+
                 Assert.NotNull(metaMetadataTableName);
 
                 var metaMetaMetadataTableName = map[metaMetadataTableName].MetaDataTableName;
 
                 Assert.Null(metaMetaMetadataTableName);
+            }
+        }
+
+        [Fact]
+        public async Task WithQuery()
+        {
+            await using (var db = await TestDatabase.CreateAsync())
+            {
+                var forcedActivity = DataManagementActivity.PersistAllNonMetaData
+                    | DataManagementActivity.PersistAllMetaDataFirstLevel;
+                var record1 = new TestDatabase.Primitives(1);
+                var record2 = new TestDatabase.Primitives(2);
+                var record3 = new TestDatabase.Primitives(3);
+
+                db.PrimitiveTable.AppendRecord(record1);
+                await db.Database.ForceDataManagementAsync(forcedActivity);
+                db.PrimitiveTable.AppendRecord(record2);
+                await db.Database.ForceDataManagementAsync(forcedActivity);
+                db.PrimitiveTable.AppendRecord(record3);
+                await db.Database.ForceDataManagementAsync(forcedActivity);
+
+                var allRecords = db.PrimitiveTable.Query()
+                    .ToImmutableArray();
+
+                Assert.Equal(3, allRecords.Length);
+                Assert.True(
+                    Enumerable.SequenceEqual([1, 2, 3], allRecords.Select(p => p.Integer)));
+
+                foreach (var record in allRecords)
+                {
+                    var queryRecords = db.PrimitiveTable.Query()
+                        .Where(pf => pf.Equal(r => r.Integer, record.Integer))
+                        .ToImmutableArray();
+
+                    Assert.Single(queryRecords);
+                    Assert.Equal(record.Integer, queryRecords.First().Integer);
+                }
+            }
+        }
+
+        [Fact]
+        public async Task WithUpdate()
+        {
+            await using (var db = await TestDatabase.CreateAsync())
+            {
+                var forcedActivity = DataManagementActivity.PersistAllNonMetaData
+                    | DataManagementActivity.PersistAllMetaDataFirstLevel;
+                var record1 = new TestDatabase.Primitives(1);
+                var record2 = new TestDatabase.Primitives(2);
+                var record3 = new TestDatabase.Primitives(3);
+                var record4 = new TestDatabase.Primitives(4);
+                var record5 = new TestDatabase.Primitives(5);
+                var record6 = new TestDatabase.Primitives(6);
+
+                db.PrimitiveTable.AppendRecord(record1);
+                await db.Database.ForceDataManagementAsync(forcedActivity);
+                db.PrimitiveTable.AppendRecord(record2);
+                await db.Database.ForceDataManagementAsync(forcedActivity);
+                db.PrimitiveTable.AppendRecord(record3);
+                await db.Database.ForceDataManagementAsync(forcedActivity);
+                db.PrimitiveTable.UpdateRecord(record1, record4);
+                await db.Database.ForceDataManagementAsync(forcedActivity);
+                db.PrimitiveTable.UpdateRecord(record2, record5);
+                await db.Database.ForceDataManagementAsync(forcedActivity);
+                db.PrimitiveTable.UpdateRecord(record3, record6);
+                await db.Database.ForceDataManagementAsync(forcedActivity);
+
+                var allRecords = db.PrimitiveTable.Query()
+                    .ToImmutableArray();
+
+                Assert.Equal(3, allRecords.Length);
+                Assert.True(
+                    Enumerable.SequenceEqual([4, 5, 6], allRecords.Select(p => p.Integer)));
+
+                foreach (var record in allRecords)
+                {
+                    var queryRecords = db.PrimitiveTable.Query()
+                        .Where(pf => pf.Equal(r => r.Integer, record.Integer))
+                        .ToImmutableArray();
+
+                    Assert.Single(queryRecords);
+                    Assert.Equal(record.Integer, queryRecords.First().Integer);
+                }
             }
         }
     }
