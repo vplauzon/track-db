b0142ec3589a263130964a77aa4165fafde0865e
vplauzon <vincent_lauzon@yahoo.com.au>
2025-11-07 18:13:58 -0500

    .

---
+++ code/TrackDb.Lib/MetadataTableSchema.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrackDb.Lib.InMemory.Block;
+
+namespace TrackDb.Lib
+{
+    internal class MetadataTableSchema : TableSchema
+    {
+        public MetadataTableSchema(
+            string tableName,
+            IImmutableList<ColumnSchema> columns)
+            : base(tableName, columns, ImmutableArray<int>.Empty, ImmutableArray<int>.Empty)
+        {
+        }
+    }
+}
