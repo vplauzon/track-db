24646d2dc7f68c688c27792395a3ea8e5ad9e0c1
vplauzon <vincent_lauzon@yahoo.com.au>
2025-11-10 12:55:25 -0500

    .

---
+++ code/TrackDb.Lib/Storage/ByteReader.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Buffers.Binary;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackDb.Lib.Storage
+{
+    /// <summary>
+    /// Byte reader:  can read bytes different ways.
+    /// </summary>
+    internal ref struct ByteReader
+    {
+        private readonly ReadOnlySpan<byte> _span;
+
+        public ByteReader(ReadOnlySpan<byte> span)
+        {
+            _span = span;
+        }
+
+        public int Position { get; private set; } = 0;
+
+        #region Into other objects
+        public ReadOnlySpan<byte> SpanForward(int length)
+        {
+            if(Position + length > _span.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(length));
+            }
+
+            var span = _span.Slice(Position, length);
+
+            Position += length;
+
+            return span;
+        }
+
+        public ReadOnlySpan<byte> RemainingSpanForward()
+        {
+            var span = _span.Slice(Position);
+
+            Position += _span.Length;
+
+            return span;
+        }
+        #endregion
+
+        #region Read operations
+        public ushort ReadUInt16()
+        {
+            var subSpan = SubSpanForwardUInt16();
+
+            return BinaryPrimitives.ReadUInt16LittleEndian(subSpan);
+        }
+
+        public long ReadInt64()
+        {
+            var subSpan = SubSpanForwardInt64();
+
+            return BinaryPrimitives.ReadInt64LittleEndian(subSpan);
+        }
+        #endregion
+
+        #region Get sub spans
+        private ReadOnlySpan<byte> SubSpanForwardUInt16()
+        {
+            return SubSpanForward(sizeof(ushort));
+        }
+
+        private ReadOnlySpan<byte> SubSpanForwardInt64()
+        {
+            return SubSpanForward(sizeof(long));
+        }
+
+        private ReadOnlySpan<byte> SubSpanForward(int length)
+        {
+            if (Position + length > _span.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(length));
+            }
+
+            var span = _span.Slice(Position, length);
+
+            Position += length;
+
+            return span;
+        }
+        #endregion
+    }
+}
