2b0ca81965ca34a06815c2f40db3e01c6e988d2e
vplauzon <vincent_lauzon@yahoo.com.au>
2025-11-12 22:22:10 -0500

    .

---
+++ code/TrackDb.Lib/ColumnSchemaProperties.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackDb.Lib
+{
+    internal readonly record struct ColumnSchemaProperties(
+        ColumnSchema ColumnSchema,
+        ColumnSchemaStat ColumnSchemaStat);
+}
