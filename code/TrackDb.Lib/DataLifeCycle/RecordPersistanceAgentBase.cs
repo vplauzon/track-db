c302813261449e6c35c47b668b2650a2e7ed3adb
vplauzon <vincent_lauzon@yahoo.com.au>
2025-11-14 20:51:26 -0500

    Breaking tests

--- code/TrackDb.Lib/DataLifeCycle/RecordPersistanceAgentBase.cs
+++ code/TrackDb.Lib/DataLifeCycle/RecordPersistanceAgentBase.cs
@@ -23,75 +23,89 @@ namespace TrackDb.Lib.DataLifeCycle
 
         public override bool Run(DataManagementActivity forcedActivity)
         {
-            var tableName = FindMergedCandidate(forcedActivity);
+            using (var tx = Database.CreateTransaction())
+            {
+                var tableName = FindMergedCandidate(forcedActivity, tx);
 
-            if (tableName != null)
-            {   //  We will persist blocks from the table
-                using (var tx = Database.CreateDummyTransaction())
+                if (tableName != null)
                 {
-                    var inMemoryDatabase = tx.TransactionState.InMemoryDatabase;
-                    var tableBlock =
-                        inMemoryDatabase.TransactionTableLogsMap[tableName].InMemoryBlocks.First();
-                    var newTableBlock = new BlockBuilder(tableBlock.TableSchema);
-                    var metadataTable =
-                        Database.GetMetaDataTable(tableBlock.TableSchema.TableName);
-                    var metaSchema = (MetadataTableSchema)metadataTable.Schema;
-                    var metadataBlock = new BlockBuilder(metadataTable.Schema);
-                    var isFirstBlockToPersist = true;
-                    var buffer = new byte[StorageManager.BlockSize];
-                    var skipRows = 0;
-
-                    newTableBlock.AppendBlock(tableBlock);
-                    newTableBlock.OrderByRecordId();
-                    while (((IBlock)newTableBlock).RecordCount - skipRows > 0)
-                    {
-                        var blockStats = newTableBlock.TruncateSerialize(buffer, skipRows);
-
-                        if (blockStats.ItemCount == 0)
-                        {
-                            throw new InvalidDataException(
-                                $"A single record is too large to persist on table " +
-                                $"'{tableBlock.TableSchema.TableName}' with " +
-                                $"{tableBlock.TableSchema.Columns.Count} columns");
-                        }
-                        //  We stop before persisting the last (typically incomplete) block
-                        if (isFirstBlockToPersist
-                            || ((IBlock)newTableBlock).RecordCount - skipRows > blockStats.ItemCount)
-                        {
-                            var blockId = Database.GetFreeBlockId();
-
-                            if (blockStats.Size > buffer.Length)
-                            {
-                                throw new InvalidOperationException(
-                                    $"Block size ({blockStats.Size}) is bigger than planned" +
-                                    $"maximum ({buffer.Length})");
-                            }
-                            StorageManager.WriteBlock(
-                                blockId,
-                                buffer.AsSpan().Slice(0, blockStats.Size));
-                            metadataBlock.AppendRecord(
-                                DateTime.Now,
-                                metadataTable.NewRecordId(),
-                                metaSchema.CreateMetadataRecord(
-                                    blockStats.ItemCount,
-                                    blockStats.Size,
-                                    blockId,
-                                    blockStats.Columns.Select(c => c.ColumnMinimum),
-                                    blockStats.Columns.Select(c => c.ColumnMaximum)));
-                            isFirstBlockToPersist = false;
-                            skipRows += blockStats.ItemCount;
-                        }
-                        else
-                        {   //  We're done
-                            break;
-                        }
-                    }
-                    newTableBlock.DeleteRecordsByRecordIndex(Enumerable.Range(0, skipRows));
-                    CommitPersistance(newTableBlock, metadataBlock, tx);
+                    PersistTable(tableName, tx);
                 }
+
+                tx.Complete();
+
+                return tableName == null;
+            }
+        }
+
+        private void PersistTable(string tableName, TransactionContext tx)
+        {   //  We persist as much blocks from the table as possible
+            var tableBlockBuilder = tx.TransactionState.UncommittedTransactionLog
+                .TransactionTableLogMap[tableName]
+                .CommittedDataBlock;
+
+            if (tableBlockBuilder == null)
+            {
+                throw new InvalidOperationException("CommittedDataBlock shouldn't be null");
             }
 
-            return tableName == null;
+            IBlock tableBlock = tableBlockBuilder;
+            var metadataTable = Database.GetMetaDataTable(tableBlock.TableSchema.TableName);
+            var metaSchema = (MetadataTableSchema)metadataTable.Schema;
+            var isFirstBlockToPersist = true;
+            var buffer = new byte[StorageManager.BlockSize];
+            var skipRows = 0;
+
+            tableBlockBuilder.OrderByRecordId();
+            while (tableBlock.RecordCount - skipRows > 0)
+            {
+                var blockStats = tableBlockBuilder.TruncateSerialize(buffer, skipRows);
+
+                if (blockStats.ItemCount == 0)
+                {
+                    throw new InvalidDataException(
+                        $"A single record is too large to persist on table " +
+                        $"'{tableBlock.TableSchema.TableName}' with " +
+                        $"{tableBlock.TableSchema.Columns.Count} columns");
+                }
+                if(blockStats.Size>buffer.Length)
+                {
+                    throw new IndexOutOfRangeException(
+                        $"Buffer overrun:  {blockStats.Size}>{buffer.Length}");
+                }
+
+                //  We stop before persisting the last (typically incomplete) block
+                if (isFirstBlockToPersist
+                    || tableBlock.RecordCount - skipRows > blockStats.ItemCount)
+                {
+                    var blockId = Database.GetFreeBlockId();
+
+                    if (blockStats.Size > buffer.Length)
+                    {
+                        throw new InvalidOperationException(
+                            $"Block size ({blockStats.Size}) is bigger than planned" +
+                            $"maximum ({buffer.Length})");
+                    }
+                    StorageManager.WriteBlock(
+                        blockId,
+                        buffer.AsSpan().Slice(0, blockStats.Size));
+                    metadataTable.AppendRecord(
+                        metaSchema.CreateMetadataRecord(
+                            blockStats.ItemCount,
+                            blockStats.Size,
+                            blockId,
+                            blockStats.Columns.Select(c => c.ColumnMinimum),
+                            blockStats.Columns.Select(c => c.ColumnMaximum)),
+                        tx);
+                    isFirstBlockToPersist = false;
+                    skipRows += blockStats.ItemCount;
+                }
+                else
+                {   //  We're done
+                    break;
+                }
+            }
+            tableBlockBuilder.DeleteRecordsByRecordIndex(Enumerable.Range(0, skipRows));
         }
 
         protected abstract int MaxInMemoryDataRecords { get; }
@@ -103,15 +117,17 @@ namespace TrackDb.Lib.DataLifeCycle
         protected abstract bool DoPersistAll(DataManagementActivity forcedActivity);
 
         #region Candidates
-        private string? FindMergedCandidate(DataManagementActivity forcedActivity)
+        private string? FindMergedCandidate(
+            DataManagementActivity forcedActivity,
+            TransactionContext tx)
         {
-            string? tableName = FindUnmergedCandidate(forcedActivity);
+            string? tableName = FindUnmergedCandidate(forcedActivity, tx);
 
             while (tableName != null)
             {
-                if (MergeTableTransactionLogs(tableName))
+                if (tx.LoadCommittedBlocksInTransaction(tableName))
                 {
-                    var newTableName = FindUnmergedCandidate(forcedActivity);
+                    var newTableName = FindUnmergedCandidate(forcedActivity, tx);
 
                     if (newTableName == tableName)
                     {
@@ -132,52 +148,49 @@ namespace TrackDb.Lib.DataLifeCycle
             return null;
         }
 
-        private string? FindUnmergedCandidate(DataManagementActivity forcedActivity)
+        private string? FindUnmergedCandidate(
+            DataManagementActivity forcedActivity,
+            TransactionContext tx)
         {
-            using (var tx = Database.CreateTransaction())
-            {
-                var inMemoryDb = tx.TransactionState.InMemoryDatabase;
+            //  Should we persist any data given the total number of records in memory (across tables)?
+            if (IsPersistanceRequired(forcedActivity, tx))
+            {   //  Find the oldest record across tables (by creation time)
+                var oldestCreationTime = DateTime.MaxValue;
+                var oldestTableName = (string?)null;
+                var buffer = new object?[1];
+                var rowIndexes = new[] { 0 };
+                var tableMap = Database.GetDatabaseStateSnapshot().TableMap;
 
-                //  Should we persist any data given the total number of records in memory (across tables)?
-                if (IsPersistanceRequired(forcedActivity, tx))
-                {   //  Find the oldest record across tables (by creation time)
-                    var oldestCreationTime = DateTime.MaxValue;
-                    var oldestTableName = (string?)null;
-                    var buffer = new object?[1];
-                    var rowIndexes = new[] { 0 };
-                    var tableMap = Database.GetDatabaseStateSnapshot().TableMap;
+                foreach (var pair in GetTableLogs(forcedActivity, tx))
+                {
+                    var tableName = pair.Key;
+                    var logs = pair.Value;
+                    var table = Database.GetAnyTable(tableName);
+                    var blocks = logs.InMemoryBlocks
+                        .Where(b => b.RecordCount > 0);
 
-                    foreach (var pair in GetTableLogs(forcedActivity, tx))
-                    {
-                        var tableName = pair.Key;
-                        var logs = pair.Value;
-                        var table = Database.GetAnyTable(tableName);
-                        var blocks = logs.InMemoryBlocks
-                            .Where(b => b.RecordCount > 0);
+                    foreach (var block in blocks)
+                    {   //  Fetch the record ID
+                        var projectedColumns =
+                            ImmutableArray.Create(block.TableSchema.CreationTimeColumnIndex);
 
-                        foreach (var block in blocks)
-                        {   //  Fetch the record ID
-                            var projectedColumns =
-                                ImmutableArray.Create(block.TableSchema.CreationTimeColumnIndex);
+                        var blockOldestCreationTime = block.Project(buffer, projectedColumns, rowIndexes, 0)
+                            .Select(r => (DateTime)r.Span[0]!)
+                            .Min();
 
-                            var blockOldestCreationTime = block.Project(buffer, projectedColumns, rowIndexes, 0)
-                                .Select(r => (DateTime)r.Span[0]!)
-                                .Min();
-
-                            if (blockOldestCreationTime < oldestCreationTime)
-                            {
-                                oldestCreationTime = blockOldestCreationTime;
-                                oldestTableName = tableName;
-                            }
+                        if (blockOldestCreationTime < oldestCreationTime)
+                        {
+                            oldestCreationTime = blockOldestCreationTime;
+                            oldestTableName = tableName;
                         }
                     }
+                }
 
-                    return oldestTableName;
-                }
-                else
-                {
-                    return null;
-                }
+                return oldestTableName;
+            }
+            else
+            {
+                return null;
             }
         }
 
@@ -194,59 +207,5 @@ namespace TrackDb.Lib.DataLifeCycle
                 || (totalRecords > 0 && DoPersistAll(forcedActivity));
         }
         #endregion
-
-        private void CommitPersistance(
-            IBlock tableBlock,
-            IBlock metadataBlock,
-            TransactionContext tx)
-        {
-            Database.ChangeDatabaseState(state =>
-            {
-                var inMemoryDatabase = state.InMemoryDatabase;
-                var tableTransactionLogsMap = inMemoryDatabase.TransactionTableLogsMap;
-                var tableLogs = tableTransactionLogsMap[tableBlock.TableSchema.TableName];
-                var metadataTableLogs = tableTransactionLogsMap.ContainsKey(metadataBlock.TableSchema.TableName)
-                ? tableTransactionLogsMap[metadataBlock.TableSchema.TableName]
-                : new ImmutableTableTransactionLogs();
-
-                //  Adjust table
-                var inMemoryBlocks = tableLogs.InMemoryBlocks.Skip(1);
-
-                inMemoryBlocks = tableBlock.RecordCount > 0
-                ? inMemoryBlocks.Prepend(tableBlock)
-                : inMemoryBlocks;
-
-                if (inMemoryBlocks.Any())
-                {
-                    tableTransactionLogsMap = tableTransactionLogsMap.SetItem(
-                        tableBlock.TableSchema.TableName,
-                        tableLogs with
-                        {
-                            InMemoryBlocks = inMemoryBlocks.ToImmutableArray()
-                        });
-                }
-                else
-                {
-                    tableTransactionLogsMap = tableTransactionLogsMap.Remove(
-                        tableBlock.TableSchema.TableName);
-                }
-                //  Adjust metadata table
-                tableTransactionLogsMap = tableTransactionLogsMap.SetItem(
-                    metadataBlock.TableSchema.TableName,
-                    metadataTableLogs with
-                    {
-                        InMemoryBlocks = metadataTableLogs.InMemoryBlocks
-                        .Append(metadataBlock)
-                        .ToImmutableArray()
-                    });
-                inMemoryDatabase = inMemoryDatabase with
-                {
-                    TransactionTableLogsMap = tableTransactionLogsMap
-                };
-                state = state with { InMemoryDatabase = inMemoryDatabase };
-
-                return state;
-            });
-        }
     }
 }
