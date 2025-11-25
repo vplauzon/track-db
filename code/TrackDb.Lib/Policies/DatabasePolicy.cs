b9b03f5b59ab823182baf858b4614c118ebfde76
vplauzon <vincent_lauzon@yahoo.com.au>
2025-11-25 09:33:57 -0500

    .

--- code/TrackDb.Lib/Policies/DatabasePolicy.cs
+++ code/TrackDb.Lib/Policies/DatabasePolicy.cs
@@ -3,19 +3,22 @@
 namespace TrackDb.Lib.Policies
 {
     public record DatabasePolicy(
+        LifeCyclePolicy LifeCyclePolicy,
         InMemoryPolicy InMemoryPolicy,
         StoragePolicy StoragePolicy,
         LogPolicy LogPolicy)
     {
         public static DatabasePolicy Create(
+            LifeCyclePolicy? LifeCyclePolicy = null,
             InMemoryPolicy? InMemoryPolicy = null,
             StoragePolicy? StoragePolicy=null,
             LogPolicy? LogPolicy = null)
         {
             return new DatabasePolicy(
-                  InMemoryPolicy ?? InMemoryPolicy.Create(),
-				  StoragePolicy ?? StoragePolicy.Create(),
-                  LogPolicy ?? LogPolicy.Create());
+                LifeCyclePolicy?? LifeCyclePolicy.Create(),
+                InMemoryPolicy ?? InMemoryPolicy.Create(),
+				StoragePolicy ?? StoragePolicy.Create(),
+                LogPolicy ?? LogPolicy.Create());
         }
     }
 }
---
+++ code/TrackDb.Lib/Policies/LifeCyclePolicy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TrackDb.Lib.Policies
+{
+    public record LifeCyclePolicy(
+        TimeSpan MaxWaitPeriod)
+    {
+        public static LifeCyclePolicy Create(
+            TimeSpan? MaxWaitPeriod = null)
+        {
+            return new LifeCyclePolicy(
+                 MaxWaitPeriod ?? TimeSpan.FromSeconds(0.5));
+        }
+    }
+}
