d282f24ea51cb85109b1d0b56dbbb6eaa8dda974
vplauzon <vincent_lauzon@yahoo.com.au>
2025-11-26 22:47:34 -0500

    .

--- code/TrackDb.Lib/DataLifeCycle/BlockMergingLogic.cs
+++ code/TrackDb.Lib/DataLifeCycle/BlockMergingLogic.cs
@@ -10,5 +10,44 @@ namespace TrackDb.Lib.DataLifeCycle
             : base(database)
         {
         }
+
+        /// <summary>
+        /// Compact block <paramref name="blockId"/> in table <paramref name="dataTableName"/>.
+        /// This block is going to be compacted and then merged with adjacent blocks.
+        /// If any block in <paramref name="otherBlockIdsToCompact"/> is encountered during
+        /// those merge operations, they are going to be compacted.  If not, they won't.
+        /// </summary>
+        /// <param name="dataTableName"></param>
+        /// <param name="blockId"></param>
+        /// <param name="otherBlockIdsToCompact"></param>
+        /// <param name="tx"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public void Compact(
+            string dataTableName,
+            int blockId,
+            IEnumerable<int> otherBlockIdsToCompact,
+            TransactionContext tx)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Merge meta block <paramref name="metaBlockId"/> in meta table
+        /// <paramref name="metaTableName"/> and compact blocks in
+        /// <paramref name="blockIdsToCompact"/> if they are encountered.
+        /// </summary>
+        /// <param name="metaTableName"></param>
+        /// <param name="metaBlockId"></param>
+        /// <param name="blockIdsToCompact"></param>
+        /// <param name="tx"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public void Merge(
+            string metaTableName,
+            int metaBlockId,
+            IEnumerable<int> blockIdsToCompact,
+            TransactionContext tx)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
