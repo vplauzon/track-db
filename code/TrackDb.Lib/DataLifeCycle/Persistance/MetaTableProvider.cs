ae7ac9ce6563a053b1a5669c1c547c306cdaef0f
vplauzon <vincent_lauzon@yahoo.com.au>
2025-12-06 07:29:51 -0500

    .

---
+++ code/TrackDb.Lib/DataLifeCycle/Persistance/MetaTableProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackDb.Lib.DataLifeCycle.Persistance
+{
+    internal class MetaTableProvider : ITableProvider
+    {
+        IEnumerable<Table> ITableProvider.GetTables(TransactionContext tx)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ code/TrackDb.Lib/DataLifeCycle/Persistance/NonMetaTableProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackDb.Lib.DataLifeCycle.Persistance
+{
+    internal class NonMetaTableProvider : ITableProvider
+    {
+        IEnumerable<Table> ITableProvider.GetTables(TransactionContext tx)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
