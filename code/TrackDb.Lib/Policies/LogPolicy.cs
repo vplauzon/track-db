041848b27fcf3dc98ba99de585fac59a92baa264
vplauzon <vincent_lauzon@yahoo.com.au>
2025-12-24 01:29:00 -0500

    .

--- code/TrackDb.Lib/Policies/LogPolicy.cs
+++ code/TrackDb.Lib/Policies/LogPolicy.cs
@@ -6,17 +6,23 @@ namespace TrackDb.Lib.Policies
     public record LogPolicy(
         StorageConfiguration? StorageConfiguration,
         TimeSpan BufferingTimeWindow,
-        int MaxBatchSizeInBytes)
+        int MaxBatchSizeInBytes,
+        int MinRecordCountBeforeCheckpoint,
+        int MinTombstonePercentBeforeCheckpoint)
     {
         public static LogPolicy Create(
             StorageConfiguration? StorageConfiguration = null,
             TimeSpan? BufferingTimeWindow = null,
-            int? MaxBatchSizeInBytes = null)
+            int? MaxBatchSizeInBytes = null,
+            int? MinRecordCountBeforeCheckpoint = null,
+            int? MinTombstonePercentBeforeCheckpoint = null)
         {
             return new LogPolicy(
                 StorageConfiguration,
                 BufferingTimeWindow ?? TimeSpan.FromSeconds(5),
-                MaxBatchSizeInBytes ?? 8 * 1024 * 1024);
+                MaxBatchSizeInBytes ?? 8 * 1024 * 1024,
+                MinRecordCountBeforeCheckpoint ?? 10000,
+                MinTombstonePercentBeforeCheckpoint ?? 40);
         }
     }
 }
