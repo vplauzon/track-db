46982ff66b8a95a637e95c532088bb19dcb94d3c
vplauzon <vincent_lauzon@yahoo.com.au>
2025-12-27 12:24:57 -0500

    .

--- code/TrackDb.Lib/DatabaseState.cs
+++ code/TrackDb.Lib/DatabaseState.cs
@@ -16,10 +16,12 @@ namespace TrackDb.Lib
     /// <param name="TableMap"></param>
     internal record DatabaseState(
         InMemoryDatabase InMemoryDatabase,
-        IImmutableDictionary<string, TableProperties> TableMap)
+        IImmutableDictionary<string, TableProperties> TableMap,
+        long AppendRecordCount,
+        long TombstoneRecordCount)
     {
         public DatabaseState(IImmutableDictionary<string, TableProperties> tableMap)
-            : this(new InMemoryDatabase(), tableMap)
+            : this(new InMemoryDatabase(), tableMap, 0, 0)
         {
         }
     }
