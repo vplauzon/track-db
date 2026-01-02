a14fadd5c2727b545f64f8e61e3c48949b8018e4
vplauzon <vincent_lauzon@yahoo.com.au>
2026-01-02 14:03:51 -0500

    .

---
+++ code/TrackDb.LogTest/Checkpoint/CheckpointTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading.Tasks;
+using TrackDb.Lib.Policies;
+using Xunit;
+
+namespace TrackDb.LogTest.Checkpoint
+{
+    public class CheckpointTest
+    {
+        private const int MAX_RECORDS = 1000;
+
+        [Fact]
+        public async Task NoneLeft()
+        {
+            var testId = $"CheckpointTest-NoneLeft-{Guid.NewGuid()}";
+
+            await using (var db = await CreateDatabaseAsync(testId))
+            {
+                var records = Enumerable.Range(0, MAX_RECORDS)
+                    .Select(i => new TestDatabase.Workflow(
+                        $"My Workflow-{i}",
+                        i,
+                        TestDatabase.WorkflowState.Started,
+                        DateTime.Now));
+
+                db.WorkflowTable.AppendRecords(records);
+                //  Delete all records
+                db.WorkflowTable.Query().Delete();
+            }
+            await using (var db = await CreateDatabaseAsync(testId))
+            {
+                var count = db.WorkflowTable.Query().Count();
+
+                Assert.Equal(0, count);
+            }
+        }
+
+        private static async Task<TestDatabase> CreateDatabaseAsync(string testId)
+        {
+            return await TestDatabase.CreateAsync(
+                testId,
+                p => p with
+                {
+                    LogPolicy = p.LogPolicy with
+                    {
+                        MinRecordCountBeforeCheckpoint = MAX_RECORDS
+                    }
+                });
+       }
+    }
+}
