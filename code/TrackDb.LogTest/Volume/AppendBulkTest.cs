b4b8a837a5a43897c1c5d9ec9acb2fd73a85e1ef
vplauzon <vincent_lauzon@yahoo.com.au>
2026-01-02 10:53:01 -0500

    Tests too long

--- code/TrackDb.LogTest/Volume/AppendBulkTest.cs
+++ code/TrackDb.LogTest/Volume/AppendBulkTest.cs
@@ -39,19 +39,19 @@ namespace TrackDb.LogTest.Volume
             await RunPerformanceTestAsync(10000);
         }
 
-        [Fact]
+        [Fact(Skip = "Too long")]
         public async Task Test0100000()
         {
             await RunPerformanceTestAsync(100000);
         }
 
-        [Fact]
+        [Fact(Skip = "Too long")]
         public async Task Test01000000()
         {
             await RunPerformanceTestAsync(1000000);
         }
 
-        [Fact]
+        [Fact(Skip = "Too long")]
         public async Task Test02000000()
         {
             await RunPerformanceTestAsync(2000000);
