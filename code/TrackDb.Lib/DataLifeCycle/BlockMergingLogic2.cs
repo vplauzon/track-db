8e0e4d5fb6a974f61b3fda68b85dc03398856331
vplauzon <vincent_lauzon@yahoo.com.au>
2026-01-12 09:04:09 -0500

    .

---
+++ code/TrackDb.Lib/DataLifeCycle/BlockMergingLogic2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackDb.Lib.DataLifeCycle
+{
+    internal class BlockMergingLogic2 : LogicBase
+    {
+        public BlockMergingLogic2(Database database)
+            : base(database)
+        {
+        }
+    }
+}
