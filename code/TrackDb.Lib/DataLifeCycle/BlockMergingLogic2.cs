e56fc10b17058e4e603e52059bdaea3c5527c411
vplauzon <vincent_lauzon@yahoo.com.au>
2026-01-12 09:13:20 -0500

    New merge API

--- code/TrackDb.Lib/DataLifeCycle/BlockMergingLogic2.cs
+++ code/TrackDb.Lib/DataLifeCycle/BlockMergingLogic2.cs
@@ -10,5 +10,22 @@ namespace TrackDb.Lib.DataLifeCycle
             : base(database)
         {
         }
+
+        /// <summary>
+        /// Merge together blocks belonging to <paramref name="metaBlockId"/>.
+        /// If <paramref name="tableName"/> is a data table, i.e. is at the lowest level,
+        /// it will compact tombstoned records.
+        /// </summary>
+        /// <param name="tableName"></param>
+        /// <param name="metaBlockId">If <c>null</c>, use in-memory meta block.</param>
+        /// <param name="tx"></param>
+        /// <returns></returns>
+        public int CompactMerge(
+            string tableName,
+            int? metaBlockId,
+            TransactionContext tx)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
