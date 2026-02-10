0c86651eddaae3d2cc627c6c3bdad1fe5aae5a4c
vplauzon <vincent_lauzon@yahoo.com.au>
2026-02-10 18:56:08 -0500

    .

---
+++ code/TrackDb.Lib/Predicate/MetaPredicateHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackDb.Lib.Predicate
+{
+    internal static class MetaPredicateHelper
+    {
+        /// <summary>
+        /// Transforms <paramref name="predicate"/> for <paramref name="schema"/> into a
+        /// predicate for <paramref name="metaSchema"/>, assuming the latter is the
+        /// metadata schema of the former.
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <param name="schema"></param>
+        /// <param name="metaSchema"></param>
+        /// <returns></returns>
+        public static QueryPredicate ToMetaData(
+            QueryPredicate predicate,
+            TableSchema schema,
+            MetadataTableSchema metaSchema)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
