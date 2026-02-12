643729c195650dac42e6b50e2527b97c52dfe8ab
vplauzon <vincent_lauzon@yahoo.com.au>
2026-02-11 19:14:17 -0500

    .

---
+++ code/TrackDb.UnitTest/QueryPredicateTests/TransformToMetadataTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TrackDb.Lib;
+using TrackDb.Lib.Predicate;
+using Xunit;
+
+namespace TrackDb.UnitTest.QueryPredicateTests
+{
+    public class TransformToMetadataTest
+    {
+        #region Inner Types
+        private record MyEntity(string Name, int Age);
+        #endregion
+
+        [Fact]
+        public void Equality()
+        {
+            var schema = TypedTableSchema<MyEntity>.FromConstructor("MyEntity");
+            var metaSchema = schema.CreateMetadataTableSchema();
+            var metaMetaSchema = metaSchema.CreateMetadataTableSchema();
+            var predicate = new BinaryOperatorPredicate(
+                schema.ColumnProperties.Index()
+                .Where(c => c.Item.ColumnSchema.ColumnName==nameof(MyEntity.Name))
+                .First()
+                .Index,
+                "John",
+                BinaryOperator.Equal);
+            var metaPredicate = MetaPredicateHelper.GetCorrespondantPredicate(
+                predicate,
+                schema,
+                metaSchema);
+            var metaMetaPredicate = MetaPredicateHelper.GetCorrespondantPredicate(
+                metaPredicate,
+                metaSchema,
+                metaMetaSchema);
+
+            Assert.Equal(2, metaPredicate.ReferencedColumnIndexes.Count());
+            Assert.Equal(2, metaMetaPredicate.ReferencedColumnIndexes.Count());
+        }
+    }
+}
