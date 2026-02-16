de1ec444f5dd23d4e3e8070d04dd2577babc53c6
vplauzon <vincent_lauzon@yahoo.com.au>
2026-02-16 02:20:53 -0500

    .

--- code/TrackDb.Lib/InMemory/ImmutableTableTransactionLogs.cs
+++ code/TrackDb.Lib/InMemory/ImmutableTableTransactionLogs.cs
@@ -59,6 +59,7 @@ namespace TrackDb.Lib.InMemory
 
             var newBlock = new BlockBuilder(InMemoryBlocks.First().TableSchema);
 
+            Console.WriteLine($"MergeLogs:  {InMemoryBlocks.Count}");
             foreach (var block in InMemoryBlocks)
             {
                 newBlock.AppendBlock(block);
