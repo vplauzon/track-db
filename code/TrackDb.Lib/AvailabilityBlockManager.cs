eb1f1fba629b36f5b78a87d13d8024903a7ecabd
vplauzon <vincent_lauzon@yahoo.com.au>
2026-02-20 15:52:01 -0500

    .

---
+++ code/TrackDb.Lib/AvailabilityBlockManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TrackDb.Lib.SystemData;
+
+namespace TrackDb.Lib
+{
+    internal class AvailabilityBlockManager
+    {
+        private readonly TypedTable<AvailableBlockRecord> _availableBlockTable;
+
+        public AvailabilityBlockManager(TypedTable<AvailableBlockRecord> availableBlockTable)
+        {
+            _availableBlockTable = availableBlockTable;
+        }
+    }
+}
