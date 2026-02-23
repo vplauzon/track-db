08e8b40af146d6f32d6cb4b87b1dc7128a7631ff
vplauzon <vincent_lauzon@yahoo.com.au>
2026-02-23 13:29:05 -0500

    .

--- code/TrackDb.Lib/CacheManager.cs
+++ code/TrackDb.Lib/CacheManager.cs
@@ -1,13 +1,21 @@
-﻿using System;
+﻿using Microsoft.Extensions.Caching.Memory;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using TrackDb.Lib.Policies;
 
 namespace TrackDb.Lib
 {
     internal class CacheManager
     {
-        public CacheManager()
+        private readonly IMemoryCache _memoryCache;
+
+        public CacheManager(BlockCachePolicy blockCachePolicy)
         {
+            _memoryCache = new MemoryCache(new MemoryCacheOptions
+            {
+                SizeLimit = blockCachePolicy.BlockCount
+            });
         }
     }
 }
