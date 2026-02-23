95a25edc0b4875a4eb044a933c0d36e66897ac46
vplauzon <vincent_lauzon@yahoo.com.au>
2026-02-23 13:18:17 -0500

    .

---
+++ code/TrackDb.Lib/CacheManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackDb.Lib
+{
+    internal class CacheManager
+    {
+        public CacheManager()
+        {
+        }
+    }
+}
