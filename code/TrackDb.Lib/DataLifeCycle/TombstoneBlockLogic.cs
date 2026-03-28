108f09818821bcd5f2171ee398a37ee540b11082
vplauzon <vincent_lauzon@yahoo.com.au>
2026-03-28 15:31:17 -0400

    .

---
+++ code/TrackDb.Lib/DataLifeCycle/TombstoneBlockLogic.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace TrackDb.Lib.DataLifeCycle
+{
+    internal class TombstoneBlockLogic : LogicBase
+    {
+        #region Inner Types
+        #endregion
+
+        public TombstoneBlockLogic(Database database)
+            : base(database)
+        {
+        }
+
+        //public bool CompactMerge(string tableName, int? metaBlockId)
+        //{
+        //}
+    }
+}
