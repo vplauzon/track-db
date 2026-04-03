ae0a3e542a157ef4d63eda114761240f42e09895
vplauzon <vincent_lauzon@yahoo.com.au>
2026-04-03 11:13:32 -0400

    .

--- code/TrackDb.Lib/DataLifeCycle/Persistance/MetaBlockMergingLogic.cs
+++ code/TrackDb.Lib/DataLifeCycle/Persistance/MetaBlockMergingLogic.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Data;
-using System.Diagnostics;
 using System.Linq;
 using TrackDb.Lib.InMemory.Block;
 
--- code/TrackDb.Lib/DataLifeCycle/Persistance/MetaMetaBlockStat.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TrackDb.Lib.DataLifeCycle.Persistance
-{
-    internal record MetaMetaBlockStat(
-        int? BlockId,
-        long MinRecordId,
-        long MaxRecordId,
-        //  This is estimated, i.e. according to min-max in tombstone tables 
-        long TombstonedRecordCount);
-}
