7ca971b0bd0c314a07213ac5bc67b8097f8bc6c3
vplauzon <vincent_lauzon@yahoo.com.au>
2026-04-10 15:04:33 -0400

    Add tests

--- code/TrackDb.UnitTest/DbTests/QueryInTest.cs
+++ code/TrackDb.UnitTest/DbTests/QueryInTest.cs
@@ -23,7 +23,12 @@ namespace TrackDb.UnitTest.DbTests
         {
             private const string MY_RECORDS_TABLE = "MyRecords";
 
-            public record MyRecord(int Integer, State State);
+            public record MyRecord(
+                int Integer,
+                State State,
+                DateTime Timestamp,
+                bool IsReady,
+                Uri BlobUri);
 
             #region Constructor
             public static async Task<MyTestDatabase> CreateAsync()
@@ -48,6 +53,10 @@ namespace TrackDb.UnitTest.DbTests
         }
         #endregion
 
+        private static readonly DateTime _timestamp1 = DateTime.Now;
+        private static readonly DateTime _timestamp2 = DateTime.Now.Subtract(TimeSpan.FromMinutes(5));
+        private static readonly DateTime _timestamp3 = DateTime.Now.Subtract(TimeSpan.FromHours(5));
+
         [Theory]
         [InlineData(false)]
         [InlineData(true)]
@@ -55,9 +64,7 @@ namespace TrackDb.UnitTest.DbTests
         {
             await using (var db = await MyTestDatabase.CreateAsync())
             {
-                db.MyRecordTable.AppendRecord(new MyTestDatabase.MyRecord(1, State.Open));
-                db.MyRecordTable.AppendRecord(new MyTestDatabase.MyRecord(2, State.Close));
-                db.MyRecordTable.AppendRecord(new MyTestDatabase.MyRecord(3, State.Undertermined));
+                InsertData(db);
 
                 await db.Database.ForceDataManagementAsync(doPushPendingData
                     ? DataManagementActivity.PersistAllNonMetaData
@@ -71,6 +78,110 @@ namespace TrackDb.UnitTest.DbTests
                 Assert.Equal(2, results.Count);
                 Assert.Contains(1, results);
                 Assert.Contains(3, results);
+            }
+        }
+
+        [Theory]
+        [InlineData(false)]
+        [InlineData(true)]
+        public async Task DateTimeIn(bool doPushPendingData)
+        {
+            await using (var db = await MyTestDatabase.CreateAsync())
+            {
+                InsertData(db);
+
+                await db.Database.ForceDataManagementAsync(doPushPendingData
+                    ? DataManagementActivity.PersistAllNonMetaData
+                    : DataManagementActivity.None);
+
+                var results = db.MyRecordTable.Query()
+                    .Where(pf => pf.In(r => r.Timestamp, [_timestamp1, _timestamp2]))
+                    .Select(r => r.Integer)
+                    .ToImmutableList();
+
+                Assert.Equal(2, results.Count);
+                Assert.Contains(1, results);
+                Assert.Contains(2, results);
+            }
+        }
+
+        [Theory]
+        [InlineData(false)]
+        [InlineData(true)]
+        public async Task BoolIn(bool doPushPendingData)
+        {
+            await using (var db = await MyTestDatabase.CreateAsync())
+            {
+                InsertData(db);
+
+                await db.Database.ForceDataManagementAsync(doPushPendingData
+                    ? DataManagementActivity.PersistAllNonMetaData
+                    : DataManagementActivity.None);
+
+                var results = db.MyRecordTable.Query()
+                    .Where(pf => pf.In(r => r.IsReady, [true]))
+                    .Select(r => r.Integer)
+                    .ToImmutableList();
+
+                Assert.Equal(2, results.Count);
+                Assert.Contains(1, results);
+                Assert.Contains(3, results);
+            }
+        }
+
+        [Theory]
+        [InlineData(false)]
+        [InlineData(true)]
+        public async Task UriIn(bool doPushPendingData)
+        {
+            await using (var db = await MyTestDatabase.CreateAsync())
+            {
+                InsertData(db);
+
+                await db.Database.ForceDataManagementAsync(doPushPendingData
+                    ? DataManagementActivity.PersistAllNonMetaData
+                    : DataManagementActivity.None);
+
+                var results = db.MyRecordTable.Query()
+                    .Where(pf => pf.In(r => r.BlobUri, [new Uri("http://Bob")]))
+                    .Select(r => r.Integer)
+                    .ToImmutableList();
+
+                Assert.Single(results);
+                Assert.Contains(2, results);
+            }
+        }
+
+        private static void InsertData(MyTestDatabase db)
+        {
+            using (var tx = db.CreateTransaction())
+            {
+                db.MyRecordTable.AppendRecord(
+                    new MyTestDatabase.MyRecord(
+                        1,
+                        State.Open,
+                        _timestamp1,
+                        true,
+                        new Uri("http://Alice")),
+                    tx);
+                db.MyRecordTable.AppendRecord(
+                    new MyTestDatabase.MyRecord(
+                        2,
+                        State.Close,
+                        _timestamp2,
+                        false,
+                        new Uri("http://Bob")),
+                    tx);
+                db.MyRecordTable.AppendRecord(
+                    new MyTestDatabase.MyRecord(
+                        3,
+                        State.Undertermined,
+                        _timestamp3,
+                        true,
+                        new Uri("http://Carl")),
+                    tx);
+
+                tx.Complete();
             }
         }
     }
