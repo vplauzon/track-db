bf6f252f9a9bfec431d1eb8ff60f9bf04816373f
vplauzon <vincent_lauzon@yahoo.com.au>
2026-04-21 10:09:18 -0400

    .

---
+++ code/TrackDb.Lib/BlockTombstones.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackDb.Lib
+{
+    internal class BlockTombstones
+    {
+        private readonly BitArray _bitmapMask;
+
+        public BlockTombstones(int blockId, string tableName, BitArray bitmapMask)
+        {
+            BlockId = blockId;
+            TableName = tableName;
+            _bitmapMask = bitmapMask;
+            DeletedCount = ComputePopCount(bitmapMask);
+        }
+
+        public int BlockId { get; }
+
+        public string TableName { get; }
+
+        public int ItemCount => _bitmapMask.Count;
+
+        public int DeletedCount { get; }
+
+        private static int ComputePopCount(BitArray bitmapMask)
+        {
+            int count = 0;
+            var bytes = new byte[(bitmapMask.Length + 7) / 8];
+
+            bitmapMask.CopyTo(bytes, 0);
+            foreach (byte b in bytes)
+            {
+                count += System.Numerics.BitOperations.PopCount(b);
+            }
+
+            return count;
+        }
+    }
+}
