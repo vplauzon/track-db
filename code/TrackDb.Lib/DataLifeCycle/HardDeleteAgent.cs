f608d62b96c848d1ed5cf3ea7b0b5269dafc814a
vplauzon <vincent_lauzon@yahoo.com.au>
2026-04-21 13:10:44 -0400

    .

---
+++ code/TrackDb.Lib/DataLifeCycle/HardDeleteAgent.cs
@@ -0,0 +1,14 @@
+﻿namespace TrackDb.Lib.DataLifeCycle
+{
+    internal class HardDeleteAgent : DataLifeCycleAgentBase
+    {
+        public HardDeleteAgent(Database database)
+            : base(database)
+        {
+        }
+
+        public override void Run(DataManagementActivity forcedDataManagementActivity)
+        {
+        }
+    }
+}
